62b1247c48d45563e7a7b1a81f7ba6260d1ca560
kangbumkyu <g.kangbumkyu@gmail.com>
2022-03-30 21:34:49 +0900

    1. Switch Components by pathname

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+function Home() {
+  return <div>Home</div>;
+}
+
+function Movie() {
+  return <div>Movie</div>;
+}
+
+function Tv() {
+  return <div>Tv</div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  if (window.location.pathname === "/") {
+    return <Home />;
+  } else if (window.location.pathname === "/movie") {
+    return <Movie />;
+  } else if (window.location.pathname === "/tv") {
+    return <Tv />;
+  }
 }
 
 export default App;
