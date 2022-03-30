86046d36622b73ce1fb18a83bbcc192fc737af65
kangbumkyu <g.kangbumkyu@gmail.com>
2022-03-30 21:38:02 +0900

    2. Add Route Component

--- src/App.js
+++ src/App.js
@@ -10,14 +10,28 @@ function Tv() {
   return <div>Tv</div>;
 }
 
-function App() {
-  if (window.location.pathname === "/") {
-    return <Home />;
-  } else if (window.location.pathname === "/movie") {
-    return <Movie />;
-  } else if (window.location.pathname === "/tv") {
-    return <Tv />;
+function Route({ path, children }) {
+  if (path === window.location.pathname) {
+    return children;
+  } else {
+    return null;
   }
+}
+
+function App() {
+  return (
+    <>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/movie">
+        <Movie />
+      </Route>
+      <Route path="/tv">
+        <Tv />
+      </Route>
+    </>
+  );
 }
 
 export default App;
